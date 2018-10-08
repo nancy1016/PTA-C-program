94cb9dbe78e614fef081c6e781b1db0de935d050
nancy1016 <liukaixa@163.com>
2018-10-08 21:37:54 +0800

    未使用pow函数。换了思路，对之前代码做了优化

--- 习题4-2 求幂级数展开的部分和/习题4-2 求幂级数展开的部分和/test.c
+++ 习题4-2 求幂级数展开的部分和/习题4-2 求幂级数展开的部分和/test.c
@@ -2,35 +2,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-double fun(double x,double n)//求数列每一项的值
-{
-	double ret = 0;
-	double  i = 0;
-	double num = 1;
-	//求分母：即阶乘
-	for (i = 1; i <=n; i++)
-	{
-		num = num*i;
-	}
-	ret = pow(x, n) / num;//数列每一项的值
-	//printf("ret=%.4f,", ret);
-	return ret;
-}
+#define eps 0.00001
 int main()
 {
 	double x = 0;
-	double sum = 0;
-	double n = 0;
+	double sum = 1;
+	double result = 1;
 	scanf("%lf", &x);
-	do
+	for (int i = 1;; i++)
 	{
-		sum = sum + fun(x, n);
-		//printf("n=%lf,sum=%.4f\n",n, sum);//加入这些输出语句都是便于查看各项值，方便调试所用
-		n++;
-
-	} while (fabs(fun(x, n)) >= 0.00001);
-	sum = sum + fun(x, n);//最后一项的绝对值必须小于0.00001，而上面的循环sum的值没有把最后一项包含进去，所以此处要添加这一句
-	//printf("n=%lf\n", n);
+		result = result*(x / i);
+		sum += result;
+		if (result < eps)
+		{
+			break;
+		}
+	}
 	printf("%.4f\n", sum);
 	system("pause");
 	return 0;
