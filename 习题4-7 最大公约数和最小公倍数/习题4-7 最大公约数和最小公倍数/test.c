a2e032115e2a917c98ba4f517b2dadd3c53448e1
nancy1016 <liukaixa@163.com>
2018-10-10 20:29:45 +0800

    使用辗转相除法求最大公约数，最小公倍数做了优化

--- 习题4-7 最大公约数和最小公倍数/习题4-7 最大公约数和最小公倍数/test.c
+++ 习题4-7 最大公约数和最小公倍数/习题4-7 最大公约数和最小公倍数/test.c
@@ -3,29 +3,48 @@
 #include<stdlib.h>
 int main()
 {
-	int M, N;
-	int yueshu = 1;
-	int beishu = 1;
+	int M, N,m,n;
+	int t = 0;
 	scanf("%d %d", &M, &N);
-	//先找到最大公约数
-	for (int i = 1; i <= M&&i <= N; i++)
+	//辗转相除法求最大公约数
+	m = M;
+	n = N;
+	while (n != 0)
 	{
-		if (M%i == 0 && N%i == 0)
-		{
-			yueshu = i;
-		}
+		t = m%n;
+		m = n;
+		n = t;
 	}
-	//根据最大公约数寻找最大公倍数
-	//beishu = yueshu;
-	for (int j = 1;; beishu= yueshu*j)
-	{
-		if (beishu%M == 0 && beishu%N == 0)
-		{
-			break;
-		}
-		j++;
-	}
-	printf("%d %d\n", yueshu, beishu);
+	//这时m是计算出的最大公约数；最小公倍数等于(M*N)/最大公约数m
+	printf("%d %d", m, M*N / m);
+
+
+
+
+
+	//int M, N;
+	//int yueshu = 1;
+	//int beishu = 1;
+	//scanf("%d %d", &M, &N);
+	////先找到最大公约数
+	//for (int i = 1; i <= M&&i <= N; i++)
+	//{
+	//	if (M%i == 0 && N%i == 0)
+	//	{
+	//		yueshu = i;
+	//	}
+	//}
+	////根据最大公约数寻找最大公倍数
+	////beishu = yueshu;
+	//for (int j = 1;; beishu= yueshu*j)
+	//{
+	//	if (beishu%M == 0 && beishu%N == 0)
+	//	{
+	//		break;
+	//	}
+	//	j++;
+	//}
+	//printf("%d %d\n", yueshu, beishu);
 	system("pause");
 	return 0;
 }
