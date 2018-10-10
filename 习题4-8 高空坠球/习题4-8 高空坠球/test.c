af2351df6e495c507c218a605b68d5245fa116cc
nancy1016 <liukaixa@163.com>
2018-10-11 00:13:37 +0800

    添加了注释

--- 习题4-8 高空坠球/习题4-8 高空坠球/test.c
+++ 习题4-8 高空坠球/习题4-8 高空坠球/test.c
@@ -4,11 +4,11 @@
 int main()
 {
 	double h=0;
-	double sum = 0;
+	double sum = 0;//在空中经过的距离
 	int n = 0;
 	scanf("%lf %d", &h, &n);
-	sum = h;
-	if (n == 0)
+	sum = h;//默认n=1时的情况
+	if (n == 0)//当第0次落地要特别处理，因为在空中经过的距离和反弹高度都为0
 	{
 		sum = 0;
 		h = 0;
@@ -19,6 +19,7 @@ int main()
 		h = h / 2;
 		sum = sum + 2*h;
 	}
+	//循环记录的是第n次落地时的高度h,求反弹高度还要计算一次
 	h = h / 2;
 	printf("%.1f %.1f\n", sum, h);
 	system("pause");
