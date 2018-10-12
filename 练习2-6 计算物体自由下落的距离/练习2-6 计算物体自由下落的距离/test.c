3f8efb9a20229dbd124cf762652b3664ee4411d6
nancy1016 <liukaixa@163.com>
2018-10-12 23:51:30 +0800

    新建项目

---
+++ 练习2-6 计算物体自由下落的距离/练习2-6 计算物体自由下落的距离/test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define G 10
+int main()
+{
+	double height=0;
+	int t = 3;
+	int g = 10;
+	height = 1.0/2 * g*(t*t);
+	printf("height =%.2f", height);
+
+	/*int t = 3;
+	printf("height=%.2f", 1.0/ 2 * G*t*t);*/
+	system("pause");
+	return 0;
+
+}
