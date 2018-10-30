4c12818f12c5549828e276564c024fa5e705bd04
nancy1016 <liukaixa@163.com>
2018-10-30 14:47:40 +0800

    新建项目

---
+++ 练习3-3 统计学生平均成绩与及格人数/练习3-3 统计学生平均成绩与及格人数/test.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	/*double sum = 0;
+	double average = 0;
+	int count = 0;
+	int n, sore;
+	scanf("%d", &n);
+	if (n > 0)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			scanf("%d", &sore);
+			sum += sore;
+			if (sore >= 60)
+			{
+				count++;
+			}
+		}
+		average = sum / n;
+	}
+	printf("average = %.1f\n", average);
+	printf("count = %d\n", count);*/
+
+
+
+	system("pause");
+	return 0;
+}
